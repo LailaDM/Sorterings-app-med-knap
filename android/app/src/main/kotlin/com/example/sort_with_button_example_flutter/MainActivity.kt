5a0e589233d28554bf7c4f6f4b0a3a6855a5296e
lail0894@edu.zealand.dk <lail0894@edu.easj.dk>
2022-12-20 22:23:13 +0100

    Sorterings App med knap

---
+++ android/app/src/main/kotlin/com/example/sort_with_button_example_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sort_with_button_example_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MainPage()));
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isDescending = false;
+  List<String> items = [
+    'Laila',
+    'Casper',
+    'Kevin',
+    'Emma',
+    'Amin',
+    'Ayaan',
+    'Malthe',
+    'Assil',
+    'Anders'
+  ];
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          // title: Text(MyApp.title),
+          centerTitle: true,
+          backgroundColor: Colors.purple,
+        ),
+        body: Column(
+          children: [
+            TextButton.icon(
+              icon: const RotatedBox(
+                quarterTurns: 1,
+                child: Icon(Icons.compare_arrows, size: 28),
+              ),
+              label: Text(
+                isDescending ? 'Decending' : 'Ascending',
+                style: const TextStyle(fontSize: 16),
+              ),
+              onPressed: () => setState(() => isDescending = !isDescending),
+            ),
+            Expanded(child: buildList()),
+          ],
+        ),
+      );
+
+  Widget buildList() => ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        final sortedItems = items
+          ..sort((item1, item2) =>
+              isDescending ? item2.compareTo(item1) : item1.compareTo(item2));
+        final item = sortedItems[index];
+
+        return ListTile(
+          leading: CircleAvatar(
+            radius: 28,
+            backgroundColor: Colors.purple,
+            //backgroundImage: NetworkImage(
+            //'https://source.unsplash.com/random?sig= $index',
+            //),
+          ),
+          title: Text(item),
+          subtitle: Text('Subtitle $index'),
+          onTap: () {},
+        );
+      });
+}
